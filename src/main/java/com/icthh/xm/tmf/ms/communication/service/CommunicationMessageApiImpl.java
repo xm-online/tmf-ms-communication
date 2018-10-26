dd5606566a57e769a906a0238ea88b0abcaff999
Ihor Shkurko <ishkurko@gmail.com>
2018-10-26 09:31:01 +0300

    API Imple service addde with 501 errors
    Signed-off-by: Ihor Shkurko <ishkurko@gmail.com>

---
+++ src/main/java/com/icthh/xm/tmf/ms/communication/service/CommunicationMessageApiImpl.java
@@ -0,0 +1,8 @@
+package com.icthh.xm.tmf.ms.communication.service;
+
+import com.icthh.xm.tmf.ms.communication.web.api.CommunicationMessageApiDelegate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommunicationMessageApiImpl implements CommunicationMessageApiDelegate {
+}
