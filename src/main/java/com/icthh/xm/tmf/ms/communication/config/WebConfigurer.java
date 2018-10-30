16958f080fa6a079ea3a5d89e851b76918ec29ab
ssenko <sergeysenja1992@gmail.com>
2018-10-30 12:47:58 +0200

    fix

--- src/main/java/com/icthh/xm/tmf/ms/communication/config/WebConfigurer.java
+++ src/main/java/com/icthh/xm/tmf/ms/communication/config/WebConfigurer.java
@@ -3,14 +3,16 @@ package com.icthh.xm.tmf.ms.communication.config;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.servlet.InstrumentedFilter;
 import com.codahale.metrics.servlets.MetricsServlet;
-import com.icthh.xm.commons.web.spring.config.XmMsWebConfiguration;
+import com.icthh.xm.commons.logging.spring.config.ServiceLoggingAspectConfiguration;
 import io.github.jhipster.config.JHipsterProperties;
 import io.undertow.UndertowOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
-import org.springframework.boot.web.server.*;
+import org.springframework.boot.web.server.MimeMappings;
+import org.springframework.boot.web.server.WebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -22,9 +24,13 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import javax.servlet.*;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.EnumSet;
 
 
 /**
@@ -32,7 +38,7 @@ import java.util.*;
  */
 @Configuration
 @Import({
-    XmMsWebConfiguration.class
+    ServiceLoggingAspectConfiguration.class
 })
 public class WebConfigurer implements ServletContextInitializer, WebServerFactoryCustomizer<WebServerFactory> {
 
