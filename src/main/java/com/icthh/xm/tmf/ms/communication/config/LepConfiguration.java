1af1d44c87e31af2089e7a2e6192d0fd1152a397
Andrew Koidan <deathangel908@gmail.com>
2018-11-19 17:45:34 +0200

    Added lep

---
+++ src/main/java/com/icthh/xm/tmf/ms/communication/config/LepConfiguration.java
@@ -0,0 +1,28 @@
+package com.icthh.xm.tmf.ms.communication.config;
+
+import com.icthh.xm.commons.lep.TenantScriptStorage;
+import com.icthh.xm.commons.lep.spring.EnableLepServices;
+import com.icthh.xm.commons.lep.spring.web.WebLepSpringConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
+
+@Configuration
+@EnableLepServices(basePackages = "your.base.package.with.services")
+public class LepConfiguration extends WebLepSpringConfiguration {
+
+    @Value("${application.lep.tenant-script-storage}")
+    private TenantScriptStorage tenantScriptStorageType;
+
+    public LepConfiguration(@Value("${spring.application.name}") String appName,
+                            ApplicationEventPublisher eventPublisher,
+                            ResourceLoader resourceLoader) {
+        super(appName, eventPublisher, resourceLoader);
+    }
+
+    @Override
+    protected TenantScriptStorage getTenantScriptStorageType() {
+        return tenantScriptStorageType;
+    }
+}
