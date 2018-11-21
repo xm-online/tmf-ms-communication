785efc812fac56c0c8a90ac31ac8e1544c7315b8
Andrew Koidan <deathangel908@gmail.com>
2018-11-21 12:43:12 +0200

    Added test for sending sms

---
+++ src/test/java/com/icthh/xm/tmf/ms/communication/config/SmppTest.java
@@ -0,0 +1,61 @@
+package com.icthh.xm.tmf.ms.communication.config;
+
+
+import com.github.dockerjava.api.command.CreateContainerCmd;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
+import com.icthh.xm.tmf.ms.communication.CommunicationApp;
+import com.icthh.xm.tmf.ms.communication.service.SmppService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.testcontainers.containers.GenericContainer;
+
+import java.util.function.Consumer;
+
+import static junit.framework.TestCase.assertTrue;
+
+@Slf4j
+@ContextConfiguration()
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {CommunicationApp.class})
+public class SmppTest {
+
+    public static final String DOCKER_IMAGE_NAME = "smpp-sim";
+    public static final int SMPP_PORT = 2775;
+
+    @Autowired
+    private SmppService service;
+
+
+    private static Consumer<CreateContainerCmd> getContainerModifier() {
+        return containerCmd -> containerCmd.withPortBindings(
+            new PortBinding(Ports.Binding.bindPort(SMPP_PORT),
+            new ExposedPort(SMPP_PORT))
+        );
+    }
+
+
+    @ClassRule
+    public static GenericContainer genericContainer = new GenericContainer(DOCKER_IMAGE_NAME)
+        .withCreateContainerCmdModifier(getContainerModifier());
+
+
+    @Test
+    public void testIsDockerUp() {
+        assertTrue(genericContainer.isRunning());
+    }
+
+    @Test
+    public void testSendingSms() {
+        service.send("test", "test");
+    }
+
+}
