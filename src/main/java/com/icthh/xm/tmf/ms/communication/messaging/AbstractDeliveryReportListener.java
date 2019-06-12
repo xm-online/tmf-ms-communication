f238cc89c40652ff7a5d8faa647f0b9d9dd9be88
ssenko <sergeysenja1992@gmail.com>
2019-06-12 17:45:00 +0300

    refactor get tags

--- src/main/java/com/icthh/xm/tmf/ms/communication/messaging/AbstractDeliveryReportListener.java
+++ src/main/java/com/icthh/xm/tmf/ms/communication/messaging/AbstractDeliveryReportListener.java
@@ -7,6 +7,7 @@ import com.icthh.xm.commons.logging.util.MdcUtils;
 import com.icthh.xm.tmf.ms.communication.service.DeliveryReportListener;
 import java.nio.charset.Charset;
 import java.util.concurrent.ExecutorService;
+import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -35,34 +36,27 @@ public abstract class AbstractDeliveryReportListener implements DeliveryReportLi
     }
 
     protected String getMessageId(DeliverSm deliverSm) {
-        OptionalParameter op = getTagValue(deliverSm, RECEIPTED_MESSAGE_ID);
-        if (op == null) {
-            return null;
-        }
-
-        byte[] value = ((OptionalParameter.OctetString) op).getValue();
-        String tagValue = new String(value, Charset.defaultCharset());
-        return StringUtils.trim(tagValue);
+        return getTagValue(deliverSm, RECEIPTED_MESSAGE_ID, op -> {
+            byte[] value = ((OptionalParameter.OctetString) op).getValue();
+            String tagValue = new String(value, Charset.defaultCharset());
+            return StringUtils.trim(tagValue);
+        });
     }
 
     protected MessageState getState(DeliverSm deliverSm) {
-        OptionalParameter op = getTagValue(deliverSm, RECEIPTED_MESSAGE_ID);
-        if (op == null) {
-            return null;
-        }
-
-        byte value = ((OptionalParameter.Byte) op).getValue();
-        return MessageState.valueOf(value);
+        return getTagValue(deliverSm, RECEIPTED_MESSAGE_ID, op -> {
+            return MessageState.valueOf(((OptionalParameter.Byte) op).getValue());
+        });
     }
 
-    protected OptionalParameter getTagValue(DeliverSm deliverSm, OptionalParameter.Tag tag) {
+    protected <T> T getTagValue(DeliverSm deliverSm, OptionalParameter.Tag tag, Function<OptionalParameter, T> converter) {
         if (deliverSm.getOptionalParameters() == null) {
             log.warn("Delivery report is not has option parameters.");
             return null;
         }
         for (OptionalParameter op : deliverSm.getOptionalParameters()) {
             if (op.tag == tag.code()) {
-                return op;
+                return converter.apply(op);
             }
         }
         return null;
