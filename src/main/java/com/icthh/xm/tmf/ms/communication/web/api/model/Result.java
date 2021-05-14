85040edd17f603b504ed10fc1f5a861b2b8da3e9
Viktor Khoroshko <victor.khoroshko@gmail.com>
2021-05-14 21:42:19 +0300

    Fix serialization issue.

--- src/main/java/com/icthh/xm/tmf/ms/communication/web/api/model/Result.java
+++ src/main/java/com/icthh/xm/tmf/ms/communication/web/api/model/Result.java
@@ -1,5 +1,6 @@
 package com.icthh.xm.tmf.ms.communication.web.api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -7,7 +8,10 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(fluent = true)
 public class Result {
+    @JsonProperty("successCount")
     private Integer successCount;
+    @JsonProperty("failureCount")
     private Integer failureCount;
+    @JsonProperty("details")
     private List<Detail> details;
 }
