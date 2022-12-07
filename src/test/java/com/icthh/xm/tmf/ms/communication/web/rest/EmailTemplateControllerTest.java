d862d20696260179bda735590f4b93cc39a3d5b8
Yaroslav <yaroslav.redko@jevera.software>
2022-12-07 19:56:58 +0200

    Email spec list api

--- src/test/java/com/icthh/xm/tmf/ms/communication/web/rest/EmailTemplateControllerTest.java
+++ src/test/java/com/icthh/xm/tmf/ms/communication/web/rest/EmailTemplateControllerTest.java
@@ -2,6 +2,9 @@ package com.icthh.xm.tmf.ms.communication.web.rest;
 
 import com.icthh.xm.tmf.ms.communication.domain.dto.RenderTemplateRequest;
 import com.icthh.xm.tmf.ms.communication.domain.dto.RenderTemplateResponse;
+import com.icthh.xm.tmf.ms.communication.domain.spec.EmailSpec;
+import com.icthh.xm.tmf.ms.communication.domain.spec.EmailTemplateSpec;
+import com.icthh.xm.tmf.ms.communication.service.EmailSpecService;
 import com.icthh.xm.tmf.ms.communication.service.SmppService;
 import com.icthh.xm.tmf.ms.communication.service.mail.EmailTemplateService;
 import lombok.SneakyThrows;
@@ -16,13 +19,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.List;
 import java.util.Map;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,6 +49,9 @@ public class EmailTemplateControllerTest {
 
     @MockBean
     private EmailTemplateService emailTemplateService;
+
+    @MockBean
+    private EmailSpecService emailSpecService;
 
     @MockBean
     private SmppService smppService;
@@ -82,6 +92,33 @@ public class EmailTemplateControllerTest {
         verifyNoInteractions(emailTemplateService);
     }
 
+    @Test
+    @SneakyThrows
+    public void getEmailSpec() {
+        List<EmailTemplateSpec> emailTemplateSpecList = List.of(
+            new EmailTemplateSpec("firstKey", "Name 1", "Subject 1", "firstKey.ftl", "{}", "{}", "{}"),
+            new EmailTemplateSpec("secondKey", "Name 2", "Subject 2", "secondKey.ftl", "{}", "{}", "{}")
+        );
+        EmailSpec emailSpec = new EmailSpec();
+        emailSpec.setEmails(emailTemplateSpecList);
+
+        when(emailSpecService.getEmailSpec()).thenReturn(emailSpec);
+
+        mockMvc.perform(get(API_BASE))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", hasSize(emailTemplateSpecList.size())))
+            .andExpect(jsonPath("$.[*].templateKey").value(containsInAnyOrder("firstKey", "secondKey")))
+            .andExpect(jsonPath("$.[*].name").value(containsInAnyOrder("Name 1", "Name 2")))
+            .andExpect(jsonPath("$.[*].subjectTemplate").value(containsInAnyOrder("Subject 1", "Subject 2")))
+            .andExpect(jsonPath("$.[*].templatePath").value(containsInAnyOrder("firstKey.ftl", "secondKey.ftl")))
+            .andExpect(jsonPath("$.[*].contextExample").value(containsInAnyOrder(two("{}"))))
+            .andExpect(jsonPath("$.[*].contextSpec").value(containsInAnyOrder(two("{}"))))
+            .andExpect(jsonPath("$.[*].contextForm").value(containsInAnyOrder(two("{}"))));
+
+        verify(emailSpecService).getEmailSpec();
+        verifyNoMoreInteractions(emailSpecService);
+    }
+
     private RenderTemplateRequest createEmailTemplateDto() {
         RenderTemplateRequest renderTemplateRequest = new RenderTemplateRequest();
         renderTemplateRequest.setContent(DEFAULT_CONTENT);
@@ -93,5 +130,9 @@ public class EmailTemplateControllerTest {
         RenderTemplateResponse renderTemplateResponse = new RenderTemplateResponse();
         renderTemplateResponse.setContent(DEFAULT_RENDERED_RESPONSE);
         return  renderTemplateResponse;
+    }
+
+    private static Object[] two(Object single) {
+        return new Object[]{single, single};
     }
 }
