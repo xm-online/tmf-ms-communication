67926a303789b9e3b8308a57d7faecaea04b488e
Serhii Senko <serhii.senko@jevera.software>
2023-01-23 12:08:57 +0200

    clean up

--- src/main/java/com/icthh/xm/tmf/ms/communication/domain/dto/TemplateDetails.java
+++ src/main/java/com/icthh/xm/tmf/ms/communication/domain/dto/TemplateDetails.java
@@ -2,8 +2,6 @@ package com.icthh.xm.tmf.ms.communication.domain.dto;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class TemplateDetails extends BaseTemplateDetails {
     private String subjectTemplate;
