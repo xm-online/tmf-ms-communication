e5a66196615065547dca80ca10c6a3347804fefb
mykhailospirych <mspirych@itsmartflex.com>
2025-04-30 13:54:55 +0300

    add possibility to disable ribbon

--- src/main/java/com/icthh/xm/tmf/ms/communication/config/RestTemplateConfiguration.java
+++ src/main/java/com/icthh/xm/tmf/ms/communication/config/RestTemplateConfiguration.java
@@ -1,13 +1,20 @@
 package com.icthh.xm.tmf.ms.communication.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Configuration
 public class RestTemplateConfiguration {
+
+    @Value("${ribbon.http.client.enabled:true}")
+    private Boolean ribbonTemplateEnabled;
 
     @Bean
     public RestTemplate restTemplate() {
@@ -16,9 +23,12 @@ public class RestTemplateConfiguration {
 
     @Bean
     @Qualifier("loadBalancedRestTemplate")
-    public RestTemplate loadBalancedRestTemplate(RestTemplateCustomizer customizer) {
+    public RestTemplate loadBalancedRestTemplate(ObjectProvider<RestTemplateCustomizer> customizerProvider) {
         RestTemplate restTemplate = new RestTemplate();
-        customizer.customize(restTemplate);
+        if (ribbonTemplateEnabled) {
+            log.info("loadBalancedRestTemplate: using Ribbon load balancer");
+            customizerProvider.ifAvailable(customizer -> customizer.customize(restTemplate));
+        }
         return restTemplate;
     }
 
