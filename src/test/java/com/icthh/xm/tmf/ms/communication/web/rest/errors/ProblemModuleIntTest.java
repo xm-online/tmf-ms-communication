b401cc6cacfd5ac1fd799d858aadc8d47c0f55db
viktorpavlenko <vitek.pavfurious@gmail.com>
2026-06-22 16:54:03 +0300

    Clean up;

--- src/test/java/com/icthh/xm/tmf/ms/communication/web/rest/errors/ProblemModuleIntTest.java
+++ src/test/java/com/icthh/xm/tmf/ms/communication/web/rest/errors/ProblemModuleIntTest.java
@@ -1,6 +1,5 @@
 package com.icthh.xm.tmf.ms.communication.web.rest.errors;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
