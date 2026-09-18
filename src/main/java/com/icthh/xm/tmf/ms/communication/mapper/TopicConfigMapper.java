db76d33d02135faf9b6350e06983bd6cc39a2e70
evgeniy.murenko <z.murenko@gmail.com>
2026-09-18 13:00:53 +0300

    added groupId property to topicKafkaQueueParamsSpec

--- src/main/java/com/icthh/xm/tmf/ms/communication/mapper/TopicConfigMapper.java
+++ src/main/java/com/icthh/xm/tmf/ms/communication/mapper/TopicConfigMapper.java
@@ -3,8 +3,16 @@ package com.icthh.xm.tmf.ms.communication.mapper;
 import com.icthh.xm.commons.topic.domain.TopicConfig;
 import com.icthh.xm.tmf.ms.communication.domain.spec.TopicKafkaQueueParamsSpec;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TopicConfigMapper {
+
+    @Mapping(target = "typeKey", ignore = true)
+    @Mapping(target = "topicName", ignore = true)
+    @Mapping(target = "metadataMaxAge", ignore = true)
+    @Mapping(target = "key", ignore = true)
+    @Mapping(target = "consumeMessagePerSecondLimit", ignore = true)
+    @Mapping(target = "autoOffsetReset", ignore = true)
     TopicConfig topicKafkaQueueParamsToConfig(TopicKafkaQueueParamsSpec topicKafkaQueueParams);
 }
